ff2dbe641b04b612caee30dc0824e8f502e324db
unknown <patjamessliney@gmail.com>
2019-07-25 00:06:18 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('.carousel').carousel()
+    interval: 2000;
+
+    // var typed = new Typed(".typed", {
+    //     strings: ["Full Stack and  Design"],
+    //     typeSpeed: 70,
+    //     loop: true,
+    //     startDelay: 1000,
+    //     showCursor: false
+    // });
+
+});
+
